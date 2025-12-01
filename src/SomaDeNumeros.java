b5f1e609fefe176ccefdecf7585214e6f423661c
Adriano de Oliveira <adreewoliver@hotmail.com>
2025-12-01 10:38:13 -0300

    update exercises

--- src/SomaDeNumeros.java
+++ src/SomaDeNumeros.java
@@ -8,7 +8,7 @@ public class SomaDeNumeros {
         double soma = 0.0; 
         double numeroAtual; 
         
-    try (Scanner scanner = new Scanner(System.in)) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("======================================");
         System.out.println("       SOMADOR DE NÚMEROS INFINITO    ");
@@ -46,6 +46,7 @@ public class SomaDeNumeros {
         System.out.println("FIM DO PROGRAMA.");
         System.out.println("A soma total dos números digitados é: " + soma);
         System.out.println("--------------------------------------");
-    }
+        
+        scanner.close();
     }
 }
